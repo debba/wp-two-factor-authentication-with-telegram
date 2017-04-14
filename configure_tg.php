1a7d1a7d37f6d4af7f76303def724c1da1f0930b
debba <andrea@debbaweb.it>
2017-04-14 02:14:03 +0200

    Release iniziale

---
+++ configure_tg.php
@@ -0,0 +1,17 @@
+<div class="section panel">
+
+	<h1><?php _e( "Configura Two Factor Authentication con Telegram" ); ?></h1>
+	<form method="post" enctype="multipart/form-data" action="options.php">
+		<hr>
+		<?php
+
+		settings_fields( 'tg_col' );
+		do_settings_sections( 'tg_col.php' );
+		?>
+		<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e( 'Save Changes' ) ?>"/>
+		</p>
+
+	</form>
+
+</div>
