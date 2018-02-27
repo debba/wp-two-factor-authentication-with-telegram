d750624e83fff09f78682c4d35e30112baaaaf9c
debba <andrea.debernardi@dueclic.com>
2018-02-27 15:20:23 +0100

    Updated README

--- two-factor-telegram.php
+++ two-factor-telegram.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: WP Two Factor Authentication with Telegram
  * Plugin URI: https://blog.dueclic.com/wordpress-autenticazione-due-fattori-telegram/
- * Description: This plugin enables two factor authentication with Telegram
+ * Description: This plugin enables two factor authentication with Telegram by increasing your website security and sends an alert every time a wrong login occurs.
  * Author: dueclic
  * Author URI: https://www.dueclic.com
  * Version: 1.4
